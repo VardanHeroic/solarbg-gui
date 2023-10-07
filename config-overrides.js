1566f9c4b9270901d3d47409070d35976cae3471
VardanHeroic <tarayanvardan@gmail.com>
2023-10-07 13:04:08 +0400

    i think it works now lol

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+//const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 module.exports = function override(config, env) {
-	config.plugins.push( new NodePolyfillPlugin() )
+//	config.plugins.push( new NodePolyfillPlugin() )
+	config.resolve.fallback = { "timers": require.resolve("timers-browserify") } 
 	
     return config
 }
